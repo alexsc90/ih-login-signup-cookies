1ec2f504134fcb278c9f16b08a31c66cb333bfda
Mike Nieva <m@mikenieva.com>
2021-02-11 19:45:40 -0600

    Listo

--- app.js
+++ app.js
@@ -41,9 +41,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
-
 // Routes middleware
 app.use('/', indexRouter);
 app.use('/', authRouter)
